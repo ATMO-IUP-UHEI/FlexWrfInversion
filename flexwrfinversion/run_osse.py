0e2f7b8600ef62283a5ecbe127fad4f72ce10514
Christopher Lüken-Winkels <christopher.lueken.winkels@gmail.com>
2025-02-05 16:09:52 +0100

    Add new loader to available loaders

--- flexwrfinversion/run_osse.py
+++ flexwrfinversion/run_osse.py
@@ -55,6 +55,7 @@ from flexwrfinversion.loaders.measurement import (
     FlexibleMeasurementLoaderTotal,
     FlexibleMeasurementLoaderTotalCo,
     MeasurementLoader,
+    MeasurementLoaderFromSingleFileTotal,
 )
 from flexwrfinversion.loaders.measurement_covariance import (
     ConstantNoCorrelation,
