30ce5b89dc63e3b705c2582874f745deb0fc65b2
Christopher Lüken-Winkels <christopher.lueken.winkels@gmail.com>
2025-02-05 16:26:28 +0100

    Add loader for prior as target

--- flexwrfinversion/run_osse.py
+++ flexwrfinversion/run_osse.py
@@ -66,6 +66,7 @@ from flexwrfinversion.loaders.measurement_covariance import (
 from flexwrfinversion.loaders.prior import (
     FlatPrior,
     FlexiblePriorLoaderTotal_ShiftToBiospheric,
+    PriorIsTarget,
     PriorLoader,
     PriorLoaderAnthBio_RelativeError_PointExtra,
     PriorLoaderAnthBioCo_RelativeError_PointExtra,
